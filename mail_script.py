2c24757d8c6d91993111bb5cc0b12dbcef6f9a8e
Juan Olvera <olverajuanluis@gmail.com>
2015-04-22 20:14:34 -0500

    mv fixed

--- mail_script.py
+++ mail_script.py
@@ -45,7 +45,10 @@ def main():
                     print 'Copying files from {0} to {1}'.format(src, dest)
 
                     # cp -r src/* dest
-                    os.system('cp -r {0}/* {1}'.format(src, dest))
+                    os.system('mv {0}/* {1}'.format(src, dest))
+
+                    # cp -r src/.* dest
+                    os.system('mv {0}/.* {1}'.format(src, dest))
                 except Exception, e:
                     print 'Error: {0}, deleting folder'.format(e)
                     shutil.rmtree(path)
